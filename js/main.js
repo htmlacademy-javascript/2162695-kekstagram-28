4eed9e674a6bfd4b4973ce825946b98f7459a4a3
Валерия Тросянова <111880353+leronana@users.noreply.github.com>
2023-03-23 14:12:33 +0400

    Отрисуй меня полностью

--- js/main.js
+++ js/main.js
@@ -1,3 +1,5 @@
 import { createPhotos } from "./data";
+import {renderThumbnails} from "./picture";
 
+renderThumbnails();
 createPhotos();
---
+++ js/picture.js
@@ -0,0 +1,22 @@
+import {createPhoto} from './data.js';
+
+const container = document.querySelector('.pictures');
+const pictureTemplate = document.querySelector('#picture').content.querySelector('.picture');
+const picturesFragment = document.createDocumentFragment();
+
+const picturesGenerated = createPhoto();
+
+picturesGenerated.forEach(({url, description, likes, comments}) => {
+  const pictureAdded = pictureTemplate.cloneNode(true);
+  pictureAdded.querySelector('.picture__img').src = url;
+  pictureAdded.querySelector('.picture__img').alt = description;
+  pictureAdded.querySelector('.picture__likes').textContent = likes;
+  pictureAdded.querySelector('.picture__comments').textContent = comments.length;
+  picturesFragment.appendChild(pictureAdded);
+
+  return pictureAdded;
+});
+
+const renderThumbnails = () => container.appendChild(picturesFragment);
+
+export {renderThumbnails};
